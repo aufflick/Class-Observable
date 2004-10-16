9811c7299cebed4589019bff1247ac3e1a019c1e
Chris Winters <chris@cwinters.com>
2004-10-16 16:55:55 +0000

    Import of CWINTERS/Class-Observable-1.04 from CPAN.
    gitpan-cpan-distribution: Class-Observable
    gitpan-cpan-version:      1.04
    gitpan-cpan-path:         CWINTERS/Class-Observable-1.04.tar.gz
    gitpan-cpan-author:       CWINTERS
    gitpan-cpan-maturity:     released

--- t/Song.pm
+++ t/Song.pm
@@ -1,29 +1,38 @@
 package Song;
 
-# $Id: Song.pm,v 1.1 2002/05/26 17:22:39 cwinters Exp $
+# $Id: Song.pm,v 1.2 2004/10/16 16:48:50 cwinters Exp $
 
 use strict;
 use base qw( Class::Observable );
 
 sub new {
-    my ( $class, $band, $name ) = @_;
-    my $self = bless( {}, $class );
-    $self->{band} = $band;
-    $self->{name} = $name;
-    return $self;
+    my ( $class, $band, $name, $log ) = @_;
+    return bless( {
+        band => $band,
+        name => $name,
+        log  => $log,
+    }, $class );
 }
 
 sub play {
     my ( $self ) = @_;
     $self->notify_observers( 'start_play' );
-    #print "Playing [$self->{name}] by [$self->{band}]\n";
+    $self->{log} &&
+        $self->{log}->( "Playing [$self->{name}] by [$self->{band}]" );
     $self->stop;
 }
 
 sub stop {
     my ( $self ) = @_;
-    #print "Stopped [$self->{name}] by [$self->{band}]\n";
+    $self->{log} &&
+        $self->{log}->( "Stopped [$self->{name}] by [$self->{band}]" );
     $self->notify_observers( 'stop_play' );
+}
+
+sub DESTROY {
+    my ( $self ) = @_;
+    $self->{log} &&
+        $self->{log}->( "Destroying '$self->{name}'" );
 }
 
 1;
